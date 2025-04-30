5478015f9cda785bd8e0ae6e0a523c9b75dd95d2
Jayanath Amaranayake <jayanath@gmail.com>
2025-04-30 21:15:06 +1000

    Removed the module. It's just adding layers without much value in this context

---
+++ traefik/terraform/traefik.tf
@@ -0,0 +1,44 @@
+resource "proxmox_lxc" "traefik" {
+  target_node  = "pmox01"
+  hostname     = "traefik"
+  ostemplate   = "local:vztmpl/ubuntu-24.04-standard_24.04-2_amd64.tar.zst"
+  ostype       = "ubuntu"
+  memory       = "2048"
+  cpulimit     = "2"
+  cores        = "2"
+  vmid         = 0 # Next available ID
+  start        = true
+  onboot       = true
+  unprivileged = true
+
+  ssh_public_keys = <<-EOT
+   ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHUEpEbTHkHDr7ayXK48FfSscRObEjZbklrb7E47IcJa lxc@1password
+  EOT
+
+  // Terraform will crash without rootfs defined
+  rootfs {
+    storage = "ceph"
+    size    = "4G"
+  }
+
+  mountpoint {
+    key     = "0"
+    slot    = 0
+    size    = "8G"
+    mp      = "/traefik-config"
+    storage = "ceph"
+  }
+
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = "192.168.193.50/32"
+    gw     = "192.168.193.1"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      mountpoint[0].storage
+    ]
+  }
+}
